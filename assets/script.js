a869f2d35f7c350db1c3fa00c52f7208158e4471
PIETRO  BONORA <0220832221023@fatectq.local>
2023-11-21 12:10:45 -0300

    atividade 2

--- assets/script.js
+++ assets/script.js
@@ -5,18 +5,22 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open')
 };
+function constraste(){
+document.body.style.setProperty("--bg-color", "#bababa");
+document.body.style.setProperty("--text-color", "#000");
+}
 
-const sr = ScrollReveal ({
-    distance: '40px',
-    duration: 2500,
-    reset: true
-});
-sr.reveal('.logo', {delay:200, origin: 'left'});
-sr.reveal('.navbar', {delay:400, origin: 'top'});
-sr.reveal('.menu-btn', {delay:520, origin: 'right'});
-sr.reveal('.home-text span', {delay:600, origin: 'top'});
-sr.reveal('.home-text h1', {delay:680, origin: 'left'});
-sr.reveal('.home-text p', {delay:750, origin: 'right'});
-sr.reveal('.main-btn', {delay:860, origin: 'left'});
-sr.reveal('.share', {delay:950, origin: 'bottom'});
-sr.reveal('.home-img', {delay:1000, origin: 'right'});
+// const sr = ScrollReveal ({
+//     distance: '40px',
+//     duration: 2500,
+//     reset: true
+// });
+// sr.reveal('.logo', {delay:200, origin: 'left'});
+// sr.reveal('.navbar', {delay:400, origin: 'top'});
+// sr.reveal('.menu-btn', {delay:520, origin: 'right'});
+// sr.reveal('.home-text span', {delay:600, origin: 'top'});
+// sr.reveal('.home-text h1', {delay:680, origin: 'left'});
+// sr.reveal('.home-text p', {delay:750, origin: 'right'});
+// sr.reveal('.main-btn', {delay:860, origin: 'left'});
+// sr.reveal('.share', {delay:950, origin: 'bottom'});
+// sr.reveal('.home-img', {delay:1000, origin: 'right'});
